fa322817386fc1d665c0139ee650d769685ce399
Raphael Schluesselbauer <raphael.schluesselbauer@leftshift.one>
2020-10-27 11:29:28 +0100

    [GAIA-2227] Rename version to tag

--- asteria-docker/src/test/groovy/one/leftshift/asteria/tasks/DockerPushTaskUnitTest.groovy
+++ asteria-docker/src/test/groovy/one/leftshift/asteria/tasks/DockerPushTaskUnitTest.groovy
@@ -22,7 +22,7 @@ class DockerPushTaskUnitTest extends Specification {
     AsteriaDockerExtension extension
 
     @Shared
-    String version
+    String tag
 
 
     void setup() {
@@ -35,13 +35,13 @@ class DockerPushTaskUnitTest extends Specification {
         extension.repositoryURI = "someRepository"
         extension.name = "some-app"
         extension.versionPrefix = "foo-"
-        version = "foo-1.2.3-SNAPSHOT"
+        tag = "foo-1.2.3-SNAPSHOT"
     }
 
     void "invokes docker build with expected values"() {
         given:
             classUnderTest = new DockerPushTask.DockerPushTaskExecution(
-                    mockedClient, extension, version)
+                    mockedClient, extension, tag)
         when:
             classUnderTest.execute()
         then:
@@ -52,7 +52,7 @@ class DockerPushTaskUnitTest extends Specification {
     void "if withLatestTag is enabled the image is pushed again with latest tag"() {
         given:
             classUnderTest = new DockerPushTask.DockerPushTaskExecution(
-                    mockedClient, extension, version)
+                    mockedClient, extension, tag)
             extension.withLatestTag = true
         when:
             classUnderTest.execute()
