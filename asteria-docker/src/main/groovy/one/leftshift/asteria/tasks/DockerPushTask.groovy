2038c26c2bab40c1d0f1d380ceed8a5b1d3c1458
Michael Mair <michael.mair@leftshift.one>
2021-05-18 15:58:29 +0200

    upgraded to gradle 6.8.3 and upgraded dependencies

--- asteria-docker/src/main/groovy/one/leftshift/asteria/tasks/DockerPushTask.groovy
+++ asteria-docker/src/main/groovy/one/leftshift/asteria/tasks/DockerPushTask.groovy
@@ -20,6 +20,7 @@ class DockerPushTask extends DefaultTask {
     AsteriaDockerExtension extension
 
     @Option(description = "Sets an explicit tag of the docker image overriding version inference and version prefix. If the explicitTag is a branch name, the according ticket number is inferred.")
+    @Input
     String explicitTag
 
     @TaskAction
